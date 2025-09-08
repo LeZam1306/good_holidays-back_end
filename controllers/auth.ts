3b7b717c0206d0ad08b956aa79009d23105c389d
Lezam <adamzamouricontact@gmail.com>
2025-09-08 16:11:46 +0200

    feat: implement user authentication with signup and login endpoints, add bcrypt for password hashing

---
+++ controllers/auth.ts
@@ -0,0 +1,73 @@
+import bcrypt from 'bcrypt';
+import type { NextFunction, Request, Response } from 'express';
+import User from '../models/user.ts';
+import { ResponseObj } from '../src/lib/responseBuilder.ts';
+
+export const signup = (req: Request, res: Response, next: NextFunction) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        ...req.body,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() =>
+          res
+            .status(201)
+            .json(ResponseObj.doResponse(false, 'Signup success', user)),
+        )
+        .catch((error) => {
+          if (error.code === 11000) {
+            res
+              .status(400)
+              .json(ResponseObj.doResponse(true, 'Email already exists', {}));
+          } else {
+            res
+              .status(500)
+              .json(ResponseObj.doResponse(true, 'Signup Failed', {}));
+          }
+        });
+    })
+    .catch(() =>
+      res.status(500).json(ResponseObj.doResponse(true, 'Signup failed', {})),
+    );
+};
+
+export const login = (req: Request, res: Response, next: NextFunction) => {
+  User.findOne({ email: req.body.email })
+    .then((userDBFind) => {
+      if (userDBFind) {
+        bcrypt
+          .compare(req.body.password, userDBFind.password)
+          .then((valid) => {
+            if (valid) {
+              res.status(200).json(
+                ResponseObj.doResponse(false, 'Authentification success', {
+                  email: userDBFind.email,
+                  token: 'TOKEN',
+                }),
+              );
+            } else
+              res
+                .status(400)
+                .json(
+                  ResponseObj.doResponse(true, 'Authentification failed', {}),
+                );
+          })
+          .catch(() =>
+            res
+              .status(400)
+              .json(
+                ResponseObj.doResponse(true, 'Authentification failed', {}),
+              ),
+          );
+      }
+    })
+    .catch(() =>
+      res
+        .status(400)
+        .json(ResponseObj.doResponse(true, 'Authentification failed', {})),
+    );
+};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import type { Application, Request, Response } from 'express';
 import express from 'express';
 import mongoose from 'mongoose';
-import User from '../models/user.ts';
+import authRoutes from '../routes/auth.ts';
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/goodholidays')
@@ -10,37 +10,6 @@ mongoose
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
-
-interface ResponseAPI {
-  error: Boolean;
-  message: string;
-  data: Object;
-}
-
-class ResponseObj {
-  private error: Boolean;
-  private message: string;
-  private data: Object;
-
-  constructor(error: boolean, message: string, data: object) {
-    this.error = error;
-    this.message = message;
-    this.data = data;
-  }
-
-  static doResponse(
-    error: Boolean,
-    message: string,
-    data: Object,
-  ): ResponseAPI {
-    const res: ResponseAPI = {
-      error: error,
-      message: message,
-      data: data,
-    };
-    return res;
-  }
-}
 
 app.use(express.json());
 
@@ -57,35 +26,7 @@ app.use((req: Request, res: Response, next) => {
   next();
 });
 
-app.post('/api/signup', (req: Request, res: Response, next) => {
-  User.findOne({ email: req.body.email })
-    .then((user) => {
-      if (user === null) {
-        const user = new User({
-          ...req.body,
-          date: '2025-09-08',
-        });
-        user
-          .save()
-          .then(() =>
-            res
-              .status(201)
-              .json(ResponseObj.doResponse(false, 'Signup success', user)),
-          )
-          .catch(() =>
-            res
-              .status(400)
-              .json(ResponseObj.doResponse(true, 'Signup Failed', {})),
-          );
-      } else
-        res
-          .status(400)
-          .json(ResponseObj.doResponse(true, 'Signup Failed !', {}));
-    })
-    .catch(() =>
-      res.status(400).json(ResponseObj.doResponse(true, 'Signup Failed !', {})),
-    );
-});
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
