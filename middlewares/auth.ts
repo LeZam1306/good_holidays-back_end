ef379adbfe1d334e1c3e4371d770cbe3ce847b5f
Lezam <adamzamouricontact@gmail.com>
2025-09-09 10:37:16 +0200

    feat: implement authentication middleware with JWT verification

---
+++ middlewares/auth.ts
@@ -0,0 +1,28 @@
+import type { NextFunction, Request, Response } from 'express';
+import Jwt from 'jsonwebtoken';
+import { ResponseObj } from '../src/lib/responseBuilder';
+
+interface AuthRequest extends Request {
+  auth?: { userId: string };
+}
+type DecodedToken = { userId: string };
+
+export const auth = (req: AuthRequest, res: Response, next: NextFunction) => {
+  const token = req.cookies.token;
+  !token &&
+    res
+      .status(400)
+      .json(ResponseObj.doResponse(true, 'User not authentified', {}));
+
+  try {
+    const decodedToken = Jwt.verify(token, 'SECRET') as DecodedToken;
+    req.auth = {
+      userId: decodedToken.userId,
+    };
+    next();
+  } catch {
+    res
+      .status(400)
+      .json(ResponseObj.doResponse(true, 'User not authentified', {}));
+  }
+};
