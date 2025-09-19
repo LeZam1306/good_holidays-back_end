12d9317f08be8c7899642cf6c86c6d613a7e67bc
Lezam <adamzamouricontact@gmail.com>
2025-09-19 13:36:58 +0200

    feat: add getNumberInvitation controller for counting pending invitations

---
+++ controllers/invitation/getNumberInvitation.ts
@@ -0,0 +1,25 @@
+import type { NextFunction, Response } from 'express';
+import { Types } from 'mongoose';
+import type { AuthRequest } from 'src/types/authRequest.interface.ts';
+import Invitation from '../../models/invitation.ts';
+import { ResponseObj } from '../../src/lib/responseBuilder.ts';
+
+export const getNumberInvitation = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  const userIdObj = new Types.ObjectId(req.auth?.userId);
+  try {
+    const count = await Invitation.countDocuments({
+      invitedId: userIdObj,
+      status: 'PENDING',
+    });
+
+    return res
+      .status(200)
+      .json(ResponseObj.doResponse(false, '', { invitationNumber: count }));
+  } catch {
+    res.status(500).json(ResponseObj.doResponse(true, 'Server error', {}));
+  }
+};
--- routes/invitation.ts
+++ routes/invitation.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { getNumberInvitation } from '../controllers/invitation/getNumberInvitation.ts';
 import { postInvitation } from '../controllers/invitation/postInvitation.ts';
 import auth from '../middlewares/auth.ts';
 import { pseudoToIdObj } from '../middlewares/pseudoToIdObj.ts';
@@ -6,5 +7,6 @@ import { pseudoToIdObj } from '../middlewares/pseudoToIdObj.ts';
 const router = express.Router();
 
 router.post('/', auth, pseudoToIdObj, postInvitation); //{eventId: string, pseudo: string}
+router.get('/count', auth, getNumberInvitation);
 
 export default router;
